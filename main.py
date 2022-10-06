b82938cadf2c73fedd77debc7887d5bc99971e04
ThaoVTP <thaovtp22@fpt.com.vn>
2022-10-06 14:37:19 +0700

    create input file

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-a = map(int, input().split())
-sorted(a)
+# a = map(int, input().split())
+# sorted(a)
 
-def minimum_step(arr):
-    return
+# def minimum_step(arr):
+#     return
+bombs = []
+maze = []
+with open('maze.txt', 'r') as f:
+    nBombs = int(f.readline())
+    for i in range(nBombs):
+        x, y, value = [int(x) for x in next(f).split()]
+        bombs.append((x, y, value))
+    for line in f:
+        maze.append(line)
+# print(nBombs)
+# for i in range(nBombs):
+#     print(bombs[i])
+# for line in maze:
+#     print(line)
+
